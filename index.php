526aaccee6cb5001d0232510ff8a5148dde9646e
Daniel Devine <dandevine0@gmail.com>
2020-10-29 08:55:21 +0000

    adds tests

--- index.php
+++ index.php
@@ -1,74 +1,7 @@
 <?php
 require_once('bootstrap.php');
 
-use Coderjerk\ElephantBird\RecentSearch;
-use Coderjerk\ElephantBird\TweetLookup;
-use Coderjerk\ElephantBird\UserLookup;
 use Coderjerk\ElephantBird\FilteredStream;
-
-// $params = [
-//     'query'        => 'football has:images ',
-//     'max_results'  => 14,
-//     'tweet.fields' => 'attachments,author_id,created_at,public_metrics,source',
-//     'expansions'   => 'attachments.media_keys',
-//     'media.fields' => 'public_metrics,type,url,width',
-// ];
-
-// $search = new RecentSearch;
-// $result = $search->RecentSearchRequest($params);
-
-// $tweets = $result->data;
-// $media  = $result->includes->media;
-
-// foreach ($tweets as $tweet) {
-//     echo $tweet->text;
-// }
-
-// foreach ($media as $medium) {
-//     echo '<img src="' . $medium->url . '">';
-// }
-
-// $ids = [
-//     '1261326399320715264',
-//     '1278347468690915330'
-// ];
-
-// $params = [
-//     'tweet.fields' => 'attachments,author_id,created_at,public_metrics,source'
-// ];
-
-// $lookup = new TweetLookup;
-// $tweets = $lookup->getTweetsById($ids, $params);
-
-
-// $ids = ['1261326399320715264'];
-
-// $lookup = new TweetLookup;
-// $tweets = $lookup->getTweetsById($ids, $params);
-
-// $params = [
-//     'user.fields' => 'id'
-// ];
-
-// $usernames = [
-//     'coderjerk'
-// ];
-
-// $userLookup = new UserLookup;
-// $user = $userLookup->lookupUsersByUsername($usernames, $params);
-
-// d($user->data);
-// echo $user->data->name;
-
-// $ids = [
-//     '802448659',
-//     '16298441'
-// ];
-
-// $userLookup = new UserLookup;
-// $user = $userLookup->lookupUsersById($ids, $params);
-
-// d($user);
 
 $params = [
     'tweet.fields' => 'attachments,author_id,created_at,public_metrics,source'
@@ -77,23 +10,25 @@ $params = [
 $filteredStream = new FilteredStream;
 
 // set a rule
-// $rules = $filteredStream->setRules('joke', 'funny times');
+$rules = $filteredStream->setRules('motorbike', 'driving is fun');
 
-//get rules
+d($rules);
+
+// //get rules
 // $rules = $filteredStream->getRules();
 
 
-//delete all rules
+// //delete all rules
 // $rules = $filteredStream->deleteAllRules();
 
-//delete one rule by id
+// //delete one rule by id
 // $rules = $filteredStream->deleteRule('1321464585245908993');
 
 // d($rules);
 
-// connect to filtered stream
-$stream = $filteredStream->connectToStream($params);
+// // connect to filtered stream
+// $stream = $filteredStream->connectToStream($params);
 
-$json = json_decode($stream);
+// $json = json_decode($stream);
 
-d($json);
+// d($json);
---
+++ tests/FilteredStreamTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Coderjerk\ElephantBird\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Coderjerk\ElephantBird\FilteredStream;
+
+use function PHPUnit\Framework\assertIsArray;
+use function PHPUnit\Framework\assertIsObject;
+
+class FilteredStreamTest extends TestCase
+{
+    public function testCanDeleteAndWriteRules()
+    {
+
+        $filteredStream = new FilteredStream;
+        //clear all rules
+        $filteredStream->deleteAllRules();
+
+        // set a rule
+        $rules = $filteredStream->setRules('test', 'testing is fun');
+        $rule = $rules->data[0];
+
+        assertIsArray($rules->data);
+
+        assertIsObject($rule);
+
+        $filteredStream->deleteAllRules();
+    }
+}
